9c586aa98c8f0cb310abd575e3339f085863d292
Tristan Bridges <mophead4@gmail.com>
2024-05-30 12:58:55 -0500

    router and server.js created and routes tested, ListItem component made with some dummy props that will need to be changed

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const shoppingRouter = require('./Router/shopping.router')
 const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
+app.use(express.urlencoded({extended: true}))
 
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
-
+app.use('/shopping', shoppingRouter)
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
     console.log('Listening on port: ', PORT);
