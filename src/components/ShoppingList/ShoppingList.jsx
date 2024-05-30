81866dd5aca0a722c278d5ab396bcd2740aff8f5
Brock Biever <brock.biever10@gmail.com>
2024-05-30 14:45:10 -0500

    incomplete

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -10,21 +10,3 @@ import React, { useState } from 'react';
 
 // create a add Item function using post that also renders the item
 
-
-<div>
-<AddGroceryForm groceryRefreshCallback={refreshGrocery} />
-{groceryList.map((groceryData, dataIndex) => {
-    return (   
-    <div key={dataIndex}>
-        <Header />
-        <h3>{groceryData.Name}</h3>
-        <p>{groceryData.Quantity}</p>
-        <p>{groceryData.Unit}</p>
-        <p>{groceryData.Purchased}</p>
-        <button onClick={() => handleClickDelete(groceryData.id)}>Delete</button>
-    </div>
-    );
-})}
-</div>
-
-
