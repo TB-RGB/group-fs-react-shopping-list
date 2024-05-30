58b43e77e8cee6e0cee25c9ab3045a94909b2d3d
Justin Kahn <jdavidkahn@gmail.com>
2024-05-30 13:10:32 -0500

    getItems and addItems

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,61 @@
-import React from 'react';
 
-import Header from '../Header/Header.jsx'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from '../Header/Header.jsx';
+import ItemForm from '../ItemForm/ItemForm.jsx';
+import ShoppingList from '../ShoppingList/ShoppingList.jsx';
 import './App.css';
 
-
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  const getItems = () => {
+    axios.get('/shopping')
+      .then(response => {
+        setItems(response.data);
+      })
+      .catch(error => {
+        console.log('Error with getItems()', error);
+      });
+  };
+
+  const addItem = (newItem) => {
+    axios.post('/shopping', newItem)
+      .then(response => {
+        setItems([items, response.data]);
+      })
+      .catch(error => {
+        console.log('Error with addItem()', error);
+      });
+  };
+
+/*
+const deleteItem=(id) =>{
+    axios.delete('/shopping/${id}')
+    .then(()={})
+}
+*/
+ 
+
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <ItemForm addItem={addItem} />
+        <ShoppingList items={items} />
+      </main>
+    </div>
+  );
 }
 
 export default App;
+
+
+
+
+
